d79278d021f495113c7b70f2cd8add9e8178458a
Matt Dudman <mattdudman95@googlemail.com>
2024-02-07 21:23:54 +0000

    Added more practice to JS.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # shiny-tribble
 This is my practice repo where I learn, try new concepts and Ideas. 
+# HELLO 
+## This is the place I try and learn new things or test them out :)
--- index.js
+++ index.js
@@ -43,3 +43,57 @@ console.log(`This is the newton value ${newton}`);
 
 // Round down the Newton temperature using the .floor() method
 // Use console.log and string interpolation to log the temperature in newton to the console
+
+// Write a JavaScript program to display the current day and time in the following format.  
+// Sample Output : Today is : Tuesday.
+// Current time is : 10 PM : 30 : 38
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let today = 'Tuesday'; 
+let time = '10:30pm'; 
+console.log(`The time is ${time} and the day is ${today}.`);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function multiple (number) {
+    // function multiple(number): This line defines a function named multiple that takes a parameter called number. Functions are blocks of reusable code.
+    if (number <= 0) {
+        return false; 
+        // if (number <= 0): This is a conditional statement. It checks whether the provided number is less than or equal to 0.
+// return false;: If the condition is true (number is less than or equal to 0), the function returns false, indicating that the number is not valid (since it should be a positive number).
+    }
+
+
+return (number % 3 === 0) || (number % 7 === 0);
+}
+// return (number % 3 === 0) || (number % 7 === 0);: This line returns true if the number is a multiple of 3 or 7. The % operator calculates the remainder when number is divided by 3 or 7.
+
+var testNumb = 1000; 
+// const testNum = 1000;: This line declares a constant variable named testNum and assigns it the value 1000.
+if (multiple(testNumb)){
+    // if (multiple(testNum)) { ... }: This checks if the result of calling the multiple function with the argument testNum is true.
+    console.log(`${testNumb} is a multiple of 3 or 7!.`);
+    // console.log(${testNum} is a multiple of 3 or 7!.');: If the condition is true, this logs a message indicating that testNum` is a multiple of 3 or 7.
+ } else {
+    // else { console.log(The number ${testNum} is not a multiple of 3 or 7.'); }: If the condition is false, this logs a message indicating that testNum` is not a multiple of 3 or 7.
+console.log(` The number ${testNumb} is not a multiple of 3 or 7.`);
+    }
+    // So, in summary, this code checks if a given positive number (testNum in this case) is a multiple of 3 or 7 and prints a corresponding message. If testNum is 1000, it will output that 1000 is not a multiple of 3 or 7.
+
+
+    function multiple (number) {
+        if (number <= 0) {
+            return false; 
+        }
+    return (number % 3 === 0) || (number % 7 === 0);
+    }
+    const testNum = 3; 
+    if (multiple(testNum)){
+        console.log(`This is now the full code snippet. ${testNum} is a multiple of 3 or 7!.`);
+     } else {
+    console.log(`This is now the full code snippet. The number ${testNum} is not a multiple of 3 or 7.`);
+        }
+
+
+
